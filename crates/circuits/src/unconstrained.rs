0093428212d442ce2ca5ae541149909f12e2ebf4
Artem Storozhuk <storojs72@gmail.com>
2024-12-06 21:07:41 +0200

    feat: u32 addition experiments

--- crates/circuits/src/unconstrained.rs
+++ crates/circuits/src/unconstrained.rs
@@ -45,6 +45,72 @@ where
 	F: TowerField + ExtensionField<FS>,
 	FS: TowerField,
 {
+	variable_u32::<U, F, FS>(builder, name, log_size, value)
+}
+
+pub fn variable_u128<U, F, FS>(
+	builder: &mut ConstraintSystemBuilder<U, F>,
+	name: impl ToString,
+	log_size: usize,
+	value: u128,
+) -> Result<OracleId, anyhow::Error>
+where
+	U: UnderlierType + Pod + PackScalar<F> + PackScalar<FS>,
+	F: TowerField + ExtensionField<FS>,
+	FS: TowerField,
+{
+	let rng = builder.add_committed(name, log_size, FS::TOWER_LEVEL);
+
+	if let Some(witness) = builder.witness() {
+		witness
+			.new_column::<FS>(rng)
+			.as_mut_slice::<u128>()
+			.into_par_iter()
+			.for_each_init(thread_rng, |_rng, data| {
+				*data = value;
+			});
+	}
+
+	Ok(rng)
+}
+
+pub fn variable_u64<U, F, FS>(
+	builder: &mut ConstraintSystemBuilder<U, F>,
+	name: impl ToString,
+	log_size: usize,
+	value: u64,
+) -> Result<OracleId, anyhow::Error>
+where
+	U: UnderlierType + Pod + PackScalar<F> + PackScalar<FS>,
+	F: TowerField + ExtensionField<FS>,
+	FS: TowerField,
+{
+	let rng = builder.add_committed(name, log_size, FS::TOWER_LEVEL);
+
+	if let Some(witness) = builder.witness() {
+		witness
+			.new_column::<FS>(rng)
+			.as_mut_slice::<u64>()
+			.into_par_iter()
+			.for_each_init(thread_rng, |_rng, data| {
+				*data = value;
+			});
+	}
+
+	Ok(rng)
+}
+
+pub fn variable_u32<U, F, FS>(
+	builder: &mut ConstraintSystemBuilder<U, F>,
+	name: impl ToString,
+	log_size: usize,
+	value: u32,
+) -> Result<OracleId, anyhow::Error>
+where
+	U: UnderlierType + Pod + PackScalar<F> + PackScalar<FS>,
+	F: TowerField + ExtensionField<FS>,
+	FS: TowerField,
+{
 	let rng = builder.add_committed(name, log_size, FS::TOWER_LEVEL);
 
 	if let Some(witness) = builder.witness() {
